cb64774fe27f91ceac9e37181aba8cdbe18ef2da
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-12-01 23:27:07 +0800

    feat: update example script to demonstrate pooltool functionality

--- hello.py
+++ hello.py
@@ -1,5 +1,26 @@
+import pooltool as pt
+
+
 def main():
-    print("Hello from ai3603-final-billiards!")
+    print("This is an example script using pooltool.")
+    print("Pooltool version:", pt.__version__)
+
+    # We need a table, some balls, and a cue ball
+    table = pt.Table.default()
+    balls = pt.get_rack(pt.GameType.NINEBALL, table)
+    cue = pt.Cue(cue_ball_id="cue")
+
+    # Wrap it up as a System
+    shot = pt.System(table=table, balls=balls, cue=cue)
+
+    # Aim at the head ball with a strong impact
+    shot.cue.set_state(V0=8, phi=pt.aim.at_ball(shot, "1"))
+
+    # Evolve the shot.
+    pt.simulate(shot, inplace=True)
+
+    # Open up the shot in the GUI
+    pt.show(shot)
 
 
 if __name__ == "__main__":
