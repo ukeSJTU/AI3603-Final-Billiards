e4561186073fa7782d74d132fba7260d36658905
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-12-01 23:48:24 +0800

    refactor(utils): remove ShotParams class definition

--- src/utils.py
+++ src/utils.py
@@ -11,14 +11,5 @@ class ShotAction(TypedDict):
     b: float  # 杆头纵向偏移（单位：球半径比例）
 
 
-class ShotParams:
-    """击球参数的合法范围"""
-
-    V0_MIN, V0_MAX = 0.5, 8.0
-    PHI_MIN, PHI_MAX = 0.0, 360.0
-    THETA_MIN, THETA_MAX = 0.0, 90.0
-    OFFSET_MIN, OFFSET_MAX = -0.5, 0.5
-
-
 BallState = dict[str, pt.objects.Ball]  # {ball_id: Ball}
 BallTargets = list[str]  # ['1', '2', ...] 目标球id列表
